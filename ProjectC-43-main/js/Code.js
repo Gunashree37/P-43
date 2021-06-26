bc4d89d94b94e25a7c411c85c549e193cdc21a3b
Gunashree S <gunashrees341@gmail.com>
2021-06-26 12:28:58 +0530

    Update Code.js

--- ProjectC-43-main/js/Code.js
+++ ProjectC-43-main/js/Code.js
@@ -3,6 +3,10 @@ const accessCode2 = "FUNCTION";
 const accessCode3 = "DATABASE";
 
 function clues() {
-    
+     fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
     
 }
